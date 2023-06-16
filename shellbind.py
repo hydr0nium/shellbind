bd6aba0571726905acc5e4e32b86a3d244de189d
Sol/Hydr0nium <anonymous@example.com>
2023-06-16 18:33:03 +0200

    Fixed a typo

--- shellbind.py
+++ shellbind.py
@@ -8,7 +8,7 @@ import readline
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-p", "--parameter", metavar="PARAMETER NAME", dest="para_name", help="The parameter the is used to run shell commands", required=True, nargs=1)
-parser.add_argument("-X" , "--method", metavar="METHOD", dest="METHOD", help="The method (GET/POST) that that is used ( Default: GET)", default="GET", nargs=1)
+parser.add_argument("-X" , "--method", metavar="METHOD", dest="method", help="The method (GET/POST) that that is used ( Default: GET)", default="GET", nargs=1)
 parser.add_argument("-u", "--host", dest="host", metavar="HOST", help="The host that is attacked. Example: http://www.victim.com/vuln.php", required=True, nargs=1)
 parser.add_argument("-D", "--debug", dest="debug", help="If set the programm does print debug messages", action='store_true', default=False)
 
